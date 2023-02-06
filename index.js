fdb5637f3b28606f62267f893ccf678ec5ba46b5
Have Samuel <samiehave9@gmail.com>
2023-02-07 01:47:46 +0300

    GetElementById

---
+++ index.js
@@ -0,0 +1,6 @@
+// grtElementById
+const content = document.getElementById('content');
+const form = document.getElementById('muffin-form');
+const btn = document.getElementById('submit');
+
+// getElementByTagName
