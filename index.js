b3466cd765e97a9e817e7c8daf017be0c1f4e155
Have Samuel <samiehave9@gmail.com>
2023-02-07 02:42:52 +0300

    Using getElementsByTagName

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const content = document.getElementById('content');
 const form = document.getElementById('muffin-form');
 const btn = document.getElementById('submit');
 
-// getElementByTagName
+// getElementsByTagName
+
