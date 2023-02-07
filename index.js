fdf70b9896468d68b43731d5aefbf9065ba6ba43
Have Samuel <samiehave9@gmail.com>
2023-02-07 04:24:05 +0300

    Modifynig the Text

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// Accessing text
 const mainGreeting = document.getElementById('main-greeting');
 
 console.log(mainGreeting.innerText);
+
+// Modifying Text
+mainGreeting.innerText = 'Hey Treasure!!';
