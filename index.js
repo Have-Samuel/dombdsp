7a4f95e65b114c54af7db184d303534d3e11dfd2
Have Samuel <samiehave9@gmail.com>
2023-02-07 03:49:22 +0300

    Searching for an element

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // grtElementById
-const content = document.getElementById('content');
-const form = document.getElementById('muffin-form');
-const btn = document.getElementById('submit');
+// const content = document.getElementById('content');
+// const form = document.getElementById('muffin-form');
+// const btn = document.getElementById('submit');
 
 // getElementsByTagName
 
