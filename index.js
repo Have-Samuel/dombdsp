1a908ace8335abc087c6572797578c40cf8542bc
Have Samuel <samiehave9@gmail.com>
2023-02-07 04:50:38 +0300

    Applying styling to the Dom

--- index.js
+++ index.js
@@ -11,3 +11,10 @@ mainGreeting.textContent = 'Tell me it changed!';
 
 // Modifying HTML
 mainGreeting.innerHTML = '<article>Life is Good!!</article>';
+
+// styling the Dom
+const mainHeading = document.querySelector('h1');
+
+mainHeading.style.color = 'teal';
+mainHeading.style.backgroundColor = 'yellow';
+
