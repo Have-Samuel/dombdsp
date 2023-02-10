e8c67e44fc68751b7d9a4c33fb984d321845b42b
Have Samuel <samiehave9@gmail.com>
2023-02-10 18:25:17 +0300

    Working with Parents, Children, and sibling

---
+++ traversal_folder/script.js
@@ -0,0 +1,10 @@
+const ul = document.querySelector('ul');
+const h1 = document.querySelector('h1');
+
+const section = ul.parentElement.parentElement;
+
+console.log(section);
+
+h1.previousElementSibling;
+
+h1.nextElementSibling.nextElementSibling;
