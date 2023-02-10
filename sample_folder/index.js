53591d6bd76e946414e7f91f0c0457a5eec8690a
Have Samuel <samiehave9@gmail.com>
2023-02-10 17:35:23 +0300

    Added an image element and prepended it to the body

--- sample_folder/index.js
+++ sample_folder/index.js
@@ -1,11 +1,24 @@
 const newLi = document.createElement('li');
 const list = document.querySelector('ul');
 const newTodo = document.createElement('li');
+const fastLi = document.createElement('li');
 const boldText = document.createElement('b');
+const newImg = document.createElement('img'); 
+
 boldText.textContent = 'DON\'T FORGET TO LOCK THE  COOP!';
 newLi.classList.add('todo');
 newTodo.classList.add('todo');
 newLi.textContent = 'Treat the chickens(Madication)';
+fastLi.textContent = 'Feed the animals';
+fastLi.className = 'todo';
+newImg.setAttribute(
+  'src',
+  'https://i.ytimg.com/vi/BgKUmzkFKUo/maxresdefault.jpg'
+);
+newImg.style.width = '500px';
 
 newTodo.append(boldText);
 list.append(newLi);
+list.append(newTodo);
+list.prepend(fastLi);
+document.body.prepend(newImg);
