da4749e99bb7a02958073ee932102b3f5301ce49
Have Samuel <samiehave9@gmail.com>
2023-02-13 17:15:12 +0300

    Using the preventDefault() method

---
+++ forms/script.js
@@ -0,0 +1,16 @@
+const formEl = document.querySelector('form');
+formEl.addEventListener('submit', (event) => {
+  event.preventDefault();
+  alert('YOU SUBMITTED THE FORM!!!');
+});
+
+const link = document.querySelector('a');
+link.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log('NO GOOGLE FOR YOU!!!');
+});
+
+const checkBox = document.querySelector('input[type= "checkbox"]');
+checkBox.addEventListener('click', (e) => {
+  console.log('NO CHECKING THIS BOX GUY!!!');
+});
