cefb6d1659d6bf748be040d257503e09de76b13c
Have Samuel <samiehave9@gmail.com>
2023-02-13 21:12:34 +0300

    Include more than one event for a given element

--- multiple-events/script.js
+++ multiple-events/script.js
@@ -1,8 +1,23 @@
 const removeButtons = document.querySelectorAll('li button');
+const form = document.querySelector('#add-friend');
+const input = document.querySelector('#first-name');
+const friendList = document.querySelector('#friend-list');
 
 // Loop removing the whole 'li'
 for (let btn of removeButtons) {
   btn.addEventListener('click', (e) => {
     e.target.parentElement.remove();
+    // console.log(e)
   })
-};
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const newFriend = document.createElement('li');
+  const btnRemover = document.createElement('button');
+  btnRemover.innerText = 'Remove';
+  newFriend.innerText = input.value;
+  input.value = '';
+  newFriend.append(btnRemover);
+  friendList.append(newFriend);
+});
