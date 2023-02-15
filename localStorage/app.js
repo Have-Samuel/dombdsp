5f0bd5082c7e250dc3c9e469b5d0ef8a8c96da63
Have Samuel <samiehave9@gmail.com>
2023-02-15 21:04:45 +0300

    Using the localStorage

--- localStorage/app.js
+++ localStorage/app.js
@@ -18,3 +18,18 @@
 // document.body.style.backgroundColor = favColor;
 
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
+
+if (localStorage.getItem('darkModeEnabled')) {
+  document.body.className = 'dark';
+  toggleSwitch.checked = true;
+}
+
+toggleSwitch.addEventListener('click', () => {
+  const { checked } = toggleSwitch;
+  if (checked) {
+    localStorage.setItem('darkModeEnabled', true);
+  } else {
+    localStorage.removeItem('darkModeEnabled');
+  }
+  document.body.className = checked ? 'dark' : '';
+});
